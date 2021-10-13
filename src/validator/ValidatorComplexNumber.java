bd62836951715d42842b99e895ab84c587e11e1b
spac-robert <spac-robert@github.com>
2021-10-13 18:33:16 +0300

    created ValidationException

--- src/validator/ValidatorComplexNumber.java
+++ src/validator/ValidatorComplexNumber.java
@@ -7,7 +7,7 @@ public class ValidatorComplexNumber {
             Double x = Double.parseDouble(tokens[0]);
             Double y = Double.parseDouble(tokens[1]);
             return true;
-        }catch (ValidationException e){
+        } catch (ValidationException | IndexOutOfBoundsException e) {
             return false;
         }
     }
