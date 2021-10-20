9dde576c7710819a24d0f177204349202c2caf9f
spac-robert <spac-robert@github.com>
2021-10-20 13:40:52 +0300

    created ValidationException

--- src/validator/ValidatorComplexNumber.java
+++ src/validator/ValidatorComplexNumber.java
@@ -2,10 +2,21 @@ package validator;
 
 public class ValidatorComplexNumber {
     public static boolean isValid(String str) {
+        boolean isok = false;
+        int nr = 0;
+        if (str.charAt(0) == '-') {
+            isok = true;
+        }
         String[] tokens = str.split("[\\Q+-\\Ei]");
         try {
-            Double x = Double.parseDouble(tokens[0]);
-            Double y = Double.parseDouble(tokens[1]);
+            if (isok) {
+                nr++;
+            }
+            Double x = Double.parseDouble(tokens[nr]);
+            Double y = Double.parseDouble(tokens[nr + 1]);
+            if (isok) {
+                x = x * (-1);
+            }
             return true;
         } catch (ValidationException | IndexOutOfBoundsException e) {
             return false;
